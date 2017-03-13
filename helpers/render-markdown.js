5b013b0236511ce0eabd7b01ae5ac9b00f37153b
Nikolas Wise <nikolaswise@gmail.com>
2017-03-13 11:51:48 -0700

    table of contents linking properly

--- helpers/render-markdown.js
+++ helpers/render-markdown.js
@@ -1,7 +1,7 @@
 var md = require('markdown-it')({
   typographer: true,
   quotes: '“”‘’'
-}).use(require('markdown-it-sanitizer'), {
+}).use(require('markdown-it-anchor')).use(require('markdown-it-sanitizer'), {
   imageClass: 'img',
   removeUnbalanced: true,
   removeUnknown: true
@@ -14,6 +14,7 @@ var typeset = require('typeset');
  * @param {String} String of Markdown
  * @returns {String} String of HTML
  */
+
 module.exports = function (text) {
   return typeset(md.render(text));
 };
